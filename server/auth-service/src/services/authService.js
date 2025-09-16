42d50a41e0b4988d1ead104883f0a9127890c917
mobin04 <mobinmathew888@gmail.com>
2025-09-16 21:39:48 +0530

    Fixing login issue

--- server/auth-service/src/services/authService.js
+++ server/auth-service/src/services/authService.js
@@ -90,10 +90,10 @@ class AuthService {
 
       user.password = undefined;
 
-      await new Email(user, '', {
-        otpSecret: otpToken,
-      }).sendLoginOtpEmail();
-      // console.log(otpToken)
+      // await new Email(user, '', {
+      //   otpSecret: otpToken,
+      // }).sendLoginOtpEmail();
+      console.log(otpToken)
 
       return formatData({ user });
     } catch (err) {
